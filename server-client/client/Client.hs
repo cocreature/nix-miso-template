4770760e20bf6b126a3759d4f7a8cd4534baf070
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-07-06 18:24:37 +0200

    Simplify client

--- server-client/client/Client.hs
+++ server-client/client/Client.hs
@@ -12,7 +12,6 @@ module Main
 import Data.Maybe
 import Data.Proxy
 import Data.Time.Format
-import Data.Time.LocalTime
 import Miso
 import Miso.String
 import Servant.API
@@ -25,75 +24,66 @@ main :: IO ()
 main = do
   currentURI <- getCurrentURI
   startApp $
-    App
-      { model = initialModel currentURI
-      , view = \m -> case runRoute (Proxy @ClientRoutes) views modelURI m of
-          Left _ -> viewHome m
-          Right v -> v
-      , update = update'
-      , initialAction = NoOp
-      , events = defaultEvents
-      , subs = [uriSub HandleURI]
-      , mountPoint = Nothing
-      }
+    App { model = initialModel currentURI
+        , initialAction = NoOp
+        , events = defaultEvents
+        , subs = [uriSub SetURI]
+        , mountPoint = Nothing
+        , update = update'
+        , view = \m -> case runRoute (Proxy @ClientRoutes) views modelURI m of
+            Left _ -> viewHome m
+            Right v -> v
+        }
+
 
 data Model = Model
   { modelURI :: URI
   , modelTime :: Maybe MisoString
-  } deriving (Show, Eq)
+  } deriving Eq
 
 initialModel :: URI -> Model
-initialModel u = Model {modelURI = u, modelTime = Nothing}
+initialModel uri = Model uri Nothing
 
 data Action
   = NoOp
-  | ChangeURI URI
-  | HandleURI URI
-  | SetTime ZonedTime
-  | SetTimeErr MisoString
   | RefreshTime
-  deriving Show
+  | SetTime MisoString
+  | ChangeURI URI
+  | SetURI URI
 
-type ClientRoutes
-   =    View Action
-   :<|> ("time" :> View Action)
+type ClientRoutes = View Action :<|> "time" :> View Action
 
 getURI :: forall a. (HasLink a, IsElem a ClientRoutes, MkLink a ~ Link) => URI
-getURI = linkURI (safeLink (Proxy :: Proxy ClientRoutes) (Proxy :: Proxy a))
-
-viewHome :: Model -> View Action
-viewHome _ =
-  div_
-    []
-    [ h1_ [] [text "Home"]
-    , button_ [onClick (ChangeURI (getURI @("time" :> View Action)))] ["View Time"]
-    ]
-
-viewTime :: Model -> View Action
-viewTime m =
-  div_
-    []
-    [ h1_ [] [text "Current Time"]
-    , div_ [] [text (fromMaybe "Time not available" (modelTime m))]
-    , button_ [onClick RefreshTime] ["Refresh Time"]
-    , button_ [onClick (ChangeURI (getURI @(View Action)))] ["Go Home"]
-    ]
+getURI = linkURI (safeLink (Proxy @ClientRoutes) (Proxy @a))
 
 views :: (Model -> View Action) :<|> (Model -> View Action)
 views = viewHome :<|> viewTime
 
-update' :: Action -> Model -> Effect Action Model
-update' NoOp m = noEff m
-update' (HandleURI u) m = m { modelURI = u} <# pure NoOp
-update' (ChangeURI u) m = m <# (pushURI u *> pure NoOp)
-update' (SetTime t) m =
-  noEff (m { modelTime = Just (ms (formatTime defaultTimeLocale "%H:%M:%S" t)) })
-update' (SetTimeErr err) m = noEff (m { modelTime = Just err })
-update' RefreshTime m = m <# do
-  timeOrErr <- runClientM getTime
-  pure $ case timeOrErr of
-    Left err -> SetTimeErr (ms (show err))
-    Right (Time time) -> SetTime time
+viewHome :: Model -> View Action
+viewHome _ =
+  div_ []
+       [ h1_ [] [text "Home"]
+       , button_ [onClick (ChangeURI (getURI @("time" :> View Action)))] ["View Time"]
+       ]
 
-getTime :: Client ClientM GetTimeAPI
-getTime = client (Proxy @GetTimeAPI)
+viewTime :: Model -> View Action
+viewTime m =
+  div_ []
+       [ h1_ [] [text "Current Time"]
+       , div_ [] [text (fromMaybe "Time not available" (modelTime m))]
+       , button_ [onClick RefreshTime] ["Refresh Time"]
+       , button_ [onClick (ChangeURI (getURI @(View Action)))] ["Go Home"]
+       ]
+
+update' :: Action -> Model -> Effect Action Model
+update' a m=
+  case a of
+    NoOp -> noEff m
+    RefreshTime -> m <# do
+      timeOrErr <- runClientM (client (Proxy @GetTimeAPI))
+      pure $ case timeOrErr of
+        Left err -> SetTime (ms (show err))
+        Right (Time t) -> SetTime (ms (formatTime defaultTimeLocale "%H:%M:%S" t))
+    SetTime t -> noEff (m { modelTime = Just t })
+    ChangeURI uri -> m <# (NoOp <$ pushURI uri)
+    SetURI uri -> noEff (m { modelURI = uri })
