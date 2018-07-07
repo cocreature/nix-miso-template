e17f6c5a86c5be352d66a93e8bfb98fefa4ddc13
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-07-07 10:02:57 +0200

    Remove demo code from final server

--- final/server/Server.hs
+++ final/server/Server.hs
@@ -37,33 +37,13 @@ main = do
   putStrLn ("Starting server on port " <> show port)
   run port $ logStdoutDev (app (optStaticDir opts))
 
-type API = "static" :> Raw :<|> GetTimeAPI :<|> ServerRoutes
-
-type ServerRoutes = ToServerRoutes ClientRoutes Wrapper Action
-
-newtype Wrapper a = Wrapper a
-
-instance ToHtml a => ToHtml (Wrapper a) where
-  toHtml (Wrapper a) =
-    doctypehtml_ $ do
-      head_ $ do
-        meta_ [charset_ "utf-8"]
-        script_ [src_ "static/all.js"] ("" :: Text)
-      body_ $ do
-        toHtml a
-  toHtmlRaw = toHtml
+type API = "static" :> Raw :<|> GetTimeAPI
 
 app :: FilePath -> Application
-app staticDir = serve (Proxy @API) (staticHandler staticDir :<|> getTimeHandler :<|> serverHandlers)
+app staticDir = serve (Proxy @API) (staticHandler staticDir :<|> getTimeHandler)
 
 staticHandler :: FilePath -> Tagged Handler Application
 staticHandler staticDir = serveDirectoryWebApp staticDir
 
 getTimeHandler :: Handler Time
 getTimeHandler = Time <$> liftIO getZonedTime
-
-serverHandlers :: Handler (Wrapper (View Action)) :<|> Handler (Wrapper (View Action))
-serverHandlers = homeHandler :<|> timeHandler
-  where homeHandler = send viewHome (getURI @(View Action))
-        timeHandler = send viewTime (getURI @("time" :> View Action))
-        send f u = pure (Wrapper (f (initialModel u)))
